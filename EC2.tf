a7e459a127e3315ab736a0cfba20bafa281f15b6
steja6872 <109953165+steja6872@users.noreply.github.com>
2025-04-30 10:45:46 -0400

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0f561d16f3799be82"  
+  ami           = "ami-0f96c63e39f9144bc"  
   instance_type = "t2.micro"
 
   tags = {
